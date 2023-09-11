3fa59412e135266a6f027eb412c2938a4a41cf52
lucasgranier <143351703+lucasgranier@users.noreply.github.com>
2023-09-11 14:47:40 -0300

    Update Exercicio3.java

--- Exercicio3.java
+++ Exercicio3.java
@@ -12,25 +12,25 @@ public class JogoDeDados {
             int dado2 = rand.nextInt(6) + 1;
             int soma = dado1 + dado2;
 
-            System.out.println("Você lançou os dados e obteve: " + dado1 + " + " + dado2 + " = " + soma);
+            System.out.println("Voce obteve: " + dado1 + " + " + dado2 + " = " + soma + "\n");
 
             if (ponto == 0) {
                 if (soma == 7 || soma == 11) {
-                    System.out.println("Você venceu!");
+                    System.out.println("Voce venceu!"+ "\n");
                     break;
                 } else if (soma == 2 || soma == 3 || soma == 12) {
-                    System.out.println("Você perdeu!");
+                    System.out.println("Voce perdeu!"+ "\n");
                     break;
                 } else {
                     ponto = soma;
-                    System.out.println("Seu ponto é " + ponto);
+                    System.out.println("ponto = " + ponto+ "\n");
                 }
             } else {
                 if (soma == ponto) {
-                    System.out.println("Você venceu ao fazer o ponto novamente!");
+                    System.out.println("Voce venceu ao fazer o ponto novamente!\n");
                     break;
                 } else if (soma == 7) {
-                    System.out.println("Você perdeu ao tirar um 7 antes do ponto!");
+                    System.out.println("Voce perdeu ao tirar um 7 antes do ponto!\n");
                     break;
                 }
             }
